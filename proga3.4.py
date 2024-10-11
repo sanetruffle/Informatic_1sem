d670c8c68f4392d2cc6a9ed4a609426674eb1fa7
sanetruffle <trufalex28@gmail.com>
2024-10-11 13:33:15 +0300

    Delete proga3.4.py

--- proga3.4.py
+++
@@ -1,7 +0,0 @@
-def draw_triangle(size, symbol):
-    for i in range(1, size+1):
-        print(symbol * i)
-
-size = int(input("Введите размер: "))
-symb = input("Введите символ: ")
-draw_triangle(size, symb)
